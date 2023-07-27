209a160d8cf2c687b5d2043b958aa96ac9f76dfa
bibersun <bibersun@gmail.com>
2023-07-27 23:51:09 +0300

    4 commit

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
 ﻿string[] input_array()
 {
+    Console.WriteLine("Введите построчно значения ячеек массива, окончание ввода - Enter на пустой строке");
     string[] tmpArr = Array.Empty<string>();
-
     while (true)
     {
-        string? s = Console.ReadLine();
-        if (s.Length is null)
+        string? sTmp = Console.ReadLine();
+        string s = sTmp is null ? String.Empty : sTmp;
+        if (s.Length == 0)
         {
-            Console.WriteLine("Пустая строка");
             return tmpArr;
         }
         else
         {
             Array.Resize(ref tmpArr, tmpArr.Length + 1);
             tmpArr[tmpArr.Length - 1] = s;
-            Console.WriteLine("Непустая строка");
         }
     }
-
 }
+string[] s = input_array();
+Console.WriteLine("Введенный массив:");
+string inputArr = "[\"" + string.Join("\", \"", s) + "\"]";
+Console.WriteLine(inputArr);
+string[] EndArray = Array.FindAll(s, x => x.Length <= 3);
 
-string[] ss = input_array();
-foreach (string s in ss)
-{
-    Console.WriteLine(s);
-}
+Console.WriteLine("Итоговый массив:");
+string strEndArr = "[\"" + string.Join("\", \"", EndArray) + "\"]";
+Console.WriteLine(strEndArr);
