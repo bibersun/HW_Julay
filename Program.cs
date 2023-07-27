ce45fbc16ab91f839d17e9888a3705f918929b0e
bibersun <bibersun@gmail.com>
2023-07-27 20:44:43 +0300

    3 commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] input_array()
+{
+    string[] tmpArr = Array.Empty<string>();
+
+    while (true)
+    {
+        string? s = Console.ReadLine();
+        if (s.Length is null)
+        {
+            Console.WriteLine("Пустая строка");
+            return tmpArr;
+        }
+        else
+        {
+            Array.Resize(ref tmpArr, tmpArr.Length + 1);
+            tmpArr[tmpArr.Length - 1] = s;
+            Console.WriteLine("Непустая строка");
+        }
+    }
+
+}
+
+string[] ss = input_array();
+foreach (string s in ss)
+{
+    Console.WriteLine(s);
+}
